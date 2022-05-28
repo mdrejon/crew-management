8b3fddac0c2cf279546694e687d91c8766b981f4
Sydur Rahman <sydurrahmant1@gmail.com>
2022-05-28 20:28:04 +0600

    requrid Pakage Added

--- config/app.php
+++ config/app.php
@@ -185,7 +185,8 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Barryvdh\Debugbar\ServiceProvider::class,
+        Yoeunes\Toastr\ToastrServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -194,6 +195,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
+        App\Providers\JetstreamServiceProvider::class,
 
     ],
 
